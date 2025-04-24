4ccd2ea484390dd078cd2b72463a4853ece264e6
Itallo Guedes <146380080+italloguedes@users.noreply.github.com>
2025-04-24 21:34:24 +0000

    id details

--- app/dashboard/atendimentos/[id]/page.tsx
+++ app/dashboard/atendimentos/[id]/page.tsx
@@ -5,7 +5,7 @@ interface PageProps {
   params: Promise<{
     id: string;
   }>;
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
