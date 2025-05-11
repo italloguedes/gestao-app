8513fe67fde08088dc6caa97b52389ba34d9e0e0
italloguedes <italo167@yahoo.com.br>
2025-05-11 10:46:06 -0300

    new reset

--- app/admin/users/layout.tsx
+++ app/admin/users/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import DashboardNav from '@/components/DashboardNav';
+import AdminGuard from '@/components/AdminGuard';
 
 export default function AdminLayout({
   children,
@@ -8,15 +9,17 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <DashboardNav />
-      <div className="py-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="bg-white shadow rounded-lg">
-            {children}
+    <AdminGuard>
+      <div className="min-h-screen bg-gray-100">
+        <DashboardNav />
+        <div className="py-6">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="bg-white shadow rounded-lg">
+              {children}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </AdminGuard>
   );
 } 
